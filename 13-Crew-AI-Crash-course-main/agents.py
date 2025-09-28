ea2d0015996f7e68d3e721348b724165847fb614
Aditya Malhotra <adml003.pro@gmail.com>
2025-09-28 17:41:31 +0530

    agents done

--- 13-Crew-AI-Crash-course-main/agents.py
+++ 13-Crew-AI-Crash-course-main/agents.py
@@ -8,6 +8,7 @@ load_dotenv()
 import os
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_MODEL_NAME"]="gpt-4-0125-preview"
+# adding up the environment variables 
 
 
 ## Create a senior blog content researcher
